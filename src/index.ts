e2d0c6d4d1dc83e46b1f8345eb4c584d7b505c36
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2022-09-29 13:35:06 +0200

    add lodash example for a non-typescript library

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import _ from 'lodash';
+
 // let names: string[];
 let names: Array<string | number>; // generic array type (a type connected to another type)
 let favoriteActivities: (string | number)[];
 favoriteActivities = ['Sports', 1, 'lol', 4];
 
 console.log(favoriteActivities);
+
+console.log(_.shuffle([1, 2, 3, 4, 5]));
