3d1ea31fc86e3168e5c65ccacac25b9d171c7b1d
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2022-10-04 16:48:19 +0200

    add comment

--- typings.d.ts
+++ typings.d.ts
@@ -1,4 +1,6 @@
+// if you plan to use md files for js-only projects you dont need this file
+// allow md files to be imported as modules
 declare module '*.md' {
 	const content: string;
-	export = content;
+	export default content;
 }
