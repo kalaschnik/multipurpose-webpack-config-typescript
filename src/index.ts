dc56fea89e4fa725331ea7e5d425479d20ff2d17
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2022-10-04 11:39:50 +0200

    test top level await

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import { myLog } from './testFunction';
 
 // let names: string[];
 let names: Array<string | number>; // generic array type (a type connected to another type)
@@ -8,3 +9,9 @@ favoriteActivities = ['Sports', 1, 'lol', 4];
 console.log(favoriteActivities);
 
 console.log(_.shuffle([1, 2, 3, 4, 5]));
+
+const response = await fetch('https://api.chucknorris.io/jokes/random');
+const data = await response.json();
+console.log(data.value);
+
+console.log(myLog('hello'));
